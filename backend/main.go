7ca638a93fb2b7f8e99e485069f1c759edd4c982
Harvey Bragg <harvey.bragg@googlemail.com>
2025-01-30 16:58:26 +0000

    Test + Initialise Stats setup

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
+	"fmt"
+	"net/http"
 )
 
 func main() {
-  http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello from Go!")
-  })
+	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from Go!")
+	})
 
-  fmt.Println("Server running on :8080")
-  http.ListenAndServe(":8080", nil)
+	fmt.Println("Server running on :8080")
+	http.ListenAndServe(":8080", nil)
 }
+
+//test
