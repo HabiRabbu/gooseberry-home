d5e158e40072a0222c9aa83461f686b5472ed8cb
Harvey Bragg <harvey.bragg@googlemail.com>
2025-01-31 19:53:49 +0000

    Setup project base - Forced Dark mode

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,8 +4,8 @@ import { CardWithForm } from "@/components/custom/CardWithForm";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-background text-foreground p-8">
-      <h1 className="text-3xl font-bold mb-6">Welcome to Gooseberry Tart</h1>
+    <main className="flex flex-col items-center justify-center min-h-screen bg-background text-foreground">
+      <h1 className="text-6xl font-bold mb-6">Gooseberry Tart.</h1>
       <CardWithForm />
     </main>
   );
